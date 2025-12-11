3aef7206444cc91a2bb460b23ce68953c64e55a9
huangshengyuan <1678818756@qq.com>
2025-12-11 15:04:23 +0800

    feat:日志初始化的时候添加异常捕捉。

--- App/Server/LogServer.cs
+++ App/Server/LogServer.cs
@@ -11,36 +11,44 @@ namespace App
 
         static LogServer()
         {
-            // 确保日志目录存在如果不存在就创建一个文件夹
-            string logDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs");
-            if (!Directory.Exists(logDirectory))
+            try
             {
-                Directory.CreateDirectory(logDirectory);
+                // 确保日志目录存在如果不存在就创建一个文件夹
+                string logDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs");
+                if (!Directory.Exists(logDirectory))
+                {
+                    Directory.CreateDirectory(logDirectory);
+                }
+
+                // 配置 Serilog
+                //_logger = new LoggerConfiguration()
+                //    .MinimumLevel.Debug()
+                //    .WriteTo.File(
+                //        path: Path.Combine(logDirectory, $"{DateTime.Now:yyyyMMdd}", "log.txt"),
+                //        rollingInterval: RollingInterval.Day, // 每天一个文件夹
+                //        restrictedToMinimumLevel: LogEventLevel.Information,
+                //        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}"
+                //    )
+                //    .WriteTo.Console() 
+                //    .CreateLogger();
+
+                _logger = new LoggerConfiguration()
+                     .MinimumLevel.Debug()
+                     .WriteTo.File(
+                         //path: Path.Combine(logDirectory, $"{DateTime.Now:yyyyMMdd}.txt"), // 直接生成日期文件
+                         path: Path.Combine(logDirectory, "log.txt"),
+                         rollingInterval: RollingInterval.Day, // 每天一个新文件
+                         restrictedToMinimumLevel: LogEventLevel.Information,
+                         outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}"
+                     )
+                     .WriteTo.Console()
+                     .CreateLogger();
             }
-
-            // 配置 Serilog
-            //_logger = new LoggerConfiguration()
-            //    .MinimumLevel.Debug()
-            //    .WriteTo.File(
-            //        path: Path.Combine(logDirectory, $"{DateTime.Now:yyyyMMdd}", "log.txt"),
-            //        rollingInterval: RollingInterval.Day, // 每天一个文件夹
-            //        restrictedToMinimumLevel: LogEventLevel.Information,
-            //        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}"
-            //    )
-            //    .WriteTo.Console() 
-            //    .CreateLogger();
-
-            _logger = new LoggerConfiguration()
-                 .MinimumLevel.Debug()
-                 .WriteTo.File(
-                        //path: Path.Combine(logDirectory, $"{DateTime.Now:yyyyMMdd}.txt"), // 直接生成日期文件
-                     path: Path.Combine(logDirectory, "log.txt"),
-                     rollingInterval: RollingInterval.Day, // 每天一个新文件
-                     restrictedToMinimumLevel: LogEventLevel.Information,
-                     outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}"
-                 )
-                 .WriteTo.Console()
-                 .CreateLogger();
+            catch (Exception ex)
+            {
+               
+                throw new Exception("日志系统初始化失败: " + ex.Message, ex);
+            }
         }
 
         // 提供静态方法供外部调用打印日志
